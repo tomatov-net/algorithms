d3498b7703d387b94d7e75201d5f3542275d3396
Alexander Savenko <alexander1729>
2021-10-19 15:09:03 +0600

    added triplets challenge

---
+++ challenges/compareTriplets/main.go
@@ -0,0 +1,36 @@
+package simpleArraySum
+
+//https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+func Validate(array []int32) bool {
+	if len(array) != 3 {
+		return false
+	}
+
+	for _, value := range array {
+		if value > 100 || value < 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Run(aliceInput []int32, bobInput []int32) []int32 {
+	result := []int32{0, 0}
+
+	if !Validate(aliceInput) || !Validate(bobInput) {
+		return result
+	}
+
+	for index, _ := range aliceInput {
+		if aliceInput[index] > bobInput[index] {
+			result[0] += 1
+		}
+		if aliceInput[index] < bobInput[index] {
+			result[1] += 1
+		}
+	}
+
+	return result
+}
---
+++ challenges/compareTriplets/main_test.go
@@ -0,0 +1,18 @@
+package simpleArraySum
+
+import (
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	testDataAlice := []int32{5, 6, 7}
+	testDataBob := []int32{3, 6, 10}
+	expectedResult := []int32{1, 1}
+	result := Run(testDataAlice, testDataBob)
+
+	for i, v := range result {
+		if v != expectedResult[i] {
+			t.Fail()
+		}
+	}
+}
