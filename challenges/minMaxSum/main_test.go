ad716abce96a9507f9c3ea442a66ba1e9691b2f1
Alexander Savenko <alexander1729>
2021-10-21 15:55:15 +0600

    - added one more task: Min Max Sum

--- challenges/minMaxSum/main_test.go
+++ challenges/minMaxSum/main_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestRun(t *testing.T) {
 	testData := []int{256741038, 623958417, 467905213, 714532089, 938071625}
-	expectedResult := []int{16, 24}
+	expectedResult := []int{2063136757, 2744467344}
 	result := Run(testData)
 
 	for key, value := range result {
